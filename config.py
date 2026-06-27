8b2970c36625db74c7458116300b7ffe6543c476
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-06-27 15:24:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,20 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+# Telegram API
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+SESSION_STRING = os.getenv("SESSION_STRING")
+
+# Bot Owner
+OWNER_ID = int(os.getenv("OWNER_ID"))
+
+# Settings
+SILENT_MODE = True
+TIMEZONE = "Asia/Kolkata"
+
+# Files
+CHANNELS_FILE = "channels.json"
+LOG_FILE = "logs/bot.log"
