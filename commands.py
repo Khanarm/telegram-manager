dcd5eabc1b43aa65fa285acf2184c5dd711b0aac
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-06-27 15:30:16 +0530

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,1 +1,51 @@
+import json
 
+CHANNELS_FILE = "channels.json"
+
+
+def load_channels():
+    try:
+        with open(CHANNELS_FILE, "r", encoding="utf-8") as f:
+            data = json.load(f)
+            return data["channels"]
+    except:
+        return []
+
+
+def save_channels(channels):
+    with open(CHANNELS_FILE, "w", encoding="utf-8") as f:
+        json.dump(
+            {
+                "channels": channels
+            },
+            f,
+            indent=4
+        )
+
+
+def add_channel(username):
+
+    channels = load_channels()
+
+    if username not in channels:
+        channels.append(username)
+        save_channels(channels)
+        return True
+
+    return False
+
+
+def remove_channel(username):
+
+    channels = load_channels()
+
+    if username in channels:
+        channels.remove(username)
+        save_channels(channels)
+        return True
+
+    return False
+
+
+def get_all_channels():
+    return load_channels()
