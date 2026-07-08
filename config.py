59c9b0eb0469d2f4e60a49dd3cfe296b738eb9d8
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-08 12:49:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,26 @@ import os
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 
-# Session string
-STRING_SESSION = os.getenv("STRING_SESSION")
+import os
+
+# Telegram API
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+
+# Master Bot
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# UserBots Sessions
+USERBOT1_SESSION = os.getenv("USERBOT1_SESSION")
+USERBOT2_SESSION = os.getenv("USERBOT2_SESSION")
+USERBOT3_SESSION = os.getenv("USERBOT3_SESSION")
+USERBOT4_SESSION = os.getenv("USERBOT4_SESSION")
 
 # Owner ID
 OWNER_ID = int(os.getenv("OWNER_ID"))
 
 # Optional
-BOT_NAME = "Channel Manager Userbot"
-VERSION = "1.0"
+BOT_NAME = "Channel Manager"
+VERSION = "2.0"
 
 MONGO_URI = os.getenv("MONGO_URI")
